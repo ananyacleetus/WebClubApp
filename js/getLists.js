492f8c92816769c8b40cf1a3e7527a3904d5d886
Ananya Cleetus <ananyacleetus@gmail.com>
2015-03-25 08:30:16 -0400

    js

---
+++ js/getLists.js
@@ -0,0 +1,16 @@
+    var getLists = function(subreddit, list) {
+        var subredditURL = 'http://www.reddit.com/r/' + subreddit + '.json';
+        $.ajax(subredditURL).done(function(data, status, xhr) {
+            if (data && data.data.children.length === 0) {
+                $(list).append('<li><h2>No posts for now!</h2><p>Check back later!</p></li>')
+            }
+            $.each(data.data.children, function(i, v) {
+                var title = v.data.title;
+                var text = v.data.selftext;
+                var id = text.substring(0, 4);
+                var name = clubarray[parseInt(id) - 1];
+                $(list).append('<li><h2>' + v.data.title + '</h2><h4>' + name + '</h4><p>' + v.data.selftext.substring(4) + '</p></li>');
+            });
+        });
+        return list;
+    };
---
+++ js/searchPosts.js
@@ -0,0 +1,15 @@
+   function searchPosts(list) {
+            var user_query = document.getElementById("search-input").value;
+            var combined = getLists('uscclubapp', '.posts.updates').concat(getLists('uscclubappcal', '.posts.calendar'));
+            var length = combined.length;
+            $(combined).each(data.data.children, function(i, v) {
+                    console.log(v.data.title);
+                }
+
+                for (var i = 0; i < length; i++) {
+                    if (combined[i].name.indexOf(user_query) > -1) {
+                        $(list).append(i);
+                }
+                }
+            
+        };
