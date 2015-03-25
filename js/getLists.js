6802408a466d89fc7ac97ea765efa7e9ad8fe51e
Ananya Cleetus <ananyacleetus@gmail.com>
2015-03-25 11:58:42 -0400

    searchfunction

--- js/getLists.js
+++ js/getLists.js
@@ -1,16 +1,23 @@
-    var getLists = function(subreddit, list, callback) {
+var getLists = function(subreddit, list) {
+        var user_query = document.getElementById("search-input").value;
+
         var subredditURL = 'http://www.reddit.com/r/' + subreddit + '.json';
         $.ajax(subredditURL).done(function(data, status, xhr) {
-            if (data && data.data.children.length === 0) {
-                $(list).append('<li><h2>No posts for now!</h2><p>Check back later!</p></li>')
-            }
-            $.each(data.data.children, function(i, v) {
-                var title = v.data.title;
-                var text = v.data.selftext;
-                var id = text.substring(0, 4);
-                var name = clubarray[parseInt(id) - 1];
-                $(list).append('<li><h2>' + v.data.title + '</h2><h4>' + name + '</h4><p>' + v.data.selftext.substring(4) + '</p></li>');
-            });
-        });
-        callback(null, $(list));
-    };
+                if (data && data.data.children.length === 0) {
+                    $(list).append('<li><h2>No posts for now!</h2><p>Check back later!</p></li>')
+                    return;
+                }
+                $.each(data.data.children, function(i, v) {
+                        var title = v.data.title;
+                        var text = v.data.selftext;
+                        var id = text.substring(0, 4);
+                        var name = clubarray[parseInt(id) - 1];
+
+                        if (name.indexOf(user_query) > -1) {
+
+                            $(list).append('<li><h2 style="font-size:20px;">' + v.data.title + '</h2><h4 style="font-size:18px;">' + name + '</h4><p style="font-size:17px;">' + v.data.selftext.substring(4) + '</p></li>');
+                        }
+
+                    );
+                });
+        };
--- js/searchPosts.js
+++ js/searchPosts.js
@@ -1,14 +1,19 @@
-function searchPosts(list){
-       var user_query = document.getElementById("search-input").value;
-       var combined = getLists('uscclubapp', '.posts.updates',callback).concat(getLists('uscclubappcal', '.posts.calendar',callback));
-       var length = combined.length;
-       $(combined).each(data.data.children, function(i, v) {
-               console.log(v.data.title);
-        });
- 
-           // for (var i = 0; i < length; i++) {
-           //     if (combined[i].name.indexOf(user_query) > -1) {
-           //         $(list).append(i);
-           //     }
-           // }
+function searchPosts() {
+
+
+    getLists('uscclubapp', 'updresults');
+    getLists('uscclubappcal', 'updcalendar');
+
+    // var user_query = document.getElementById("search-input").value;
+    // var combined = getLists('uscclubapp', '.posts.updates',callback).concat(getLists('uscclubappcal', '.posts.calendar',callback));
+    // var length = combined.length;
+    // $(combined).each(data.data.children, function(i, v) {
+    //         console.log(v.data.title);
+    //  });
+
+    // for (var i = 0; i < length; i++) {
+    //     if (combined[i].name.indexOf(user_query) > -1) {
+    //         $(list).append(i);
+    //     }
+    // }
 }
