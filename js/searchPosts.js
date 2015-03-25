938bc81bbfe675e0590ed4349d5a924fecd15239
Ananya W Cleetus <ananyacleetus@gmail.com>
2015-03-25 11:18:27 -0400

    Update searchPosts.js

--- js/searchPosts.js
+++ js/searchPosts.js
@@ -1,6 +1,6 @@
 function searchPosts(list){
        var user_query = document.getElementById("search-input").value;
-       var combined = getLists('uscclubapp', '.posts.updates').concat(getLists('uscclubappcal', '.posts.calendar'));
+       var combined = getLists('uscclubapp', '.posts.updates',callback).concat(getLists('uscclubappcal', '.posts.calendar',callback));
        var length = combined.length;
        $(combined).each(data.data.children, function(i, v) {
                console.log(v.data.title);
